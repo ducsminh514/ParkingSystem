c7e374dfaf340776c90ba88c24e1a60514d35dc4
xuuu <voquocanh1405@gmail.com>
2025-10-20 15:30:56 +0700

    kk

--- ParkingSystem.Client/Program.cs
+++ ParkingSystem.Client/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Components.Web;
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ParkingSystem.Client;
 using ParkingSystem.Client.Services;
@@ -9,6 +11,14 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// ✨ Thêm LocalStorage
+builder.Services.AddBlazoredLocalStorage();
+
+// ✨ Thêm Authorization
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, SimpleAuthStateProvider>();
+builder.Services.AddScoped<SimpleAuthStateProvider>();
 builder.Services.AddScoped<ParkingService>();
 // Program.cs (Server)
 //builder.Services.AddCors(options =>
